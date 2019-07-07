7b392c65895b9ec7b125853b43cc50a20d055f32
Lautaro Mónaco <lauti.monaco@hotmail.com>
2019-07-07 14:16:30 -0300

    07/07

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var NumeroUno;
+    var NumeroDos;
+
+    NumeroUno = document.getElementById("numeroUno").value;
+    NumeroDos = document.getElementById("numeroDos").value;
+
+    var NumeroUno = parseInt(numeroUno);
+    var NumeroUno = parseInt(numeroDos);
+
+    alert("La suma es " NumeroUno + NumeroDos);
+
 
 }
 
