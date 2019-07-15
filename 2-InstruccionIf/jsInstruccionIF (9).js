ea51e4f4f1f60f28cac859f1cd8b4c6a064222d8
unknown <lauti.monaco@hotmail.com>
2019-07-15 10:46:49 -0300

    Ejercicio 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,10 +1,9 @@
 function mostrar()
 {
 	var aleatorio 
-	
-	aleatorio = document.getElementById(aleatorio).value =
-	Math.floor(Math.random() * 10) + 1;
-	alert("aleatorio");
+
+	aleatorio = Math.floor(Math.random() * 10) + 1;
+	alert(aleatorio);
 }
 //Genero el número RANDOM entre 1 y 10 
 //FIN DE LA FUNCIÓN	
