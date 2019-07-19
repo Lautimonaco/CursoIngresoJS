00b59ad29cebc39beb5425f7eae8add2438ac515
Lautaro Mónaco <lauti.monaco@hotmail.com>
2019-07-19 11:58:29 -0300

    Add files via upload

---
+++ jsIteraciones (2).js
@@ -0,0 +1,13 @@
+function mostrar() {
+	
+	var numeros;
+	numeros = 10;
+	numeros = parseInt(numeros);
+
+	while (numeros >= 0) {
+		numeros = numeros - 1;
+		alert(numeros);
+	}
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsIteraciones (3).js
@@ -0,0 +1,11 @@
+function mostrar() {
+
+    var clave = prompt("ingrese el número clave.");
+
+    while (!(clave == "utn750")) {
+       
+        clave == prompt("Error, ingrese la clave nuevamente");
+        break;
+    }
+
+}//FIN DE LA FUNCIÓN
