a69d2c01dfbc86ab2a46dabfb2eeeae57422ed04
Lautaro Mónaco <lauti.monaco@hotmail.com>
2019-07-19 11:59:35 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,38 @@
-function mostrar()
-{
-	alert('iteración while');
+function mostrar() {
+
+	var numeros;
+	numeros = 0;
+	numeros = parseInt(numeros);
+
+	while (numeros < 10) {
+		numeros = numeros + 1;
+		alert(numeros);   /* funciona con true o 1 */
+	}                                              /* Sin break funciona infinito */
+}
+
+/* Diferentes While:
+
+Contador = 0
+
+while (contador > 5) {
+	contador = contador + 1
+}
+
+-Validacion:
+
+var nota;
+nota = 10
+
+while (nota > 10) {
+	console.log("error");
+	nota = prompt("ingrese nombre")  ---Si no colocamos esto, se repite infinitas veces.---
+}
+
+ var respuesta = "S"
+while (Respuesta == "S") {
 
 
-}//FIN DE LA FUNCIÓN
+	Respuesta = prompt("S para seguir");
+}
+
+*/
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,13 @@
-function mostrar()
-{
-	alert('iteración while');
+function mostrar() {
+	
+	var numeros;
+	numeros = 10;
+	numeros = parseInt(numeros);
+
+	while (numeros >= 0) {
+		numeros = numeros - 1;
+		alert(numeros);
+	}
 
 
 }//FIN DE LA FUNCIÓN
