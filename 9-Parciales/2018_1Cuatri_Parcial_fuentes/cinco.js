94081a131bf73759b1cdd917d078baa254f0435e
Lautaro Mónaco <lauti.monaco@hotmail.com>
2019-07-19 12:00:10 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,35 @@
-function mostrar()
-{
+function mostrar() {
+
+    var sistemaSolar;
+    var mensaje;
+
+
+    sistemaSolar = prompt("Ingrese un planeta: ");
+    alert(sistemaSolar);
+
+    switch(sistemaSolar) {
+        case "mercurio":
+        case "venus":
+            mensaje =("acá hace mas calor");
+            break;
+
+        case "tierra":
+            mensaje =("acá vivimos");
+            break;
+
+        case "jupiter":
+        case "saturno":
+        case "pluton":
+        case "urano":
+        case "neptuno":
+        case "marte":
+            mensaje =("acá hace más frio");
+            break;
+
+        default;
+            mensaje =("No es un planeta valido");
+
+            alert(mensaje);
+    }
 
 }
