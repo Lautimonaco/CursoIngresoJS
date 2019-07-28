ce0209d47bea3fb389c4907984b32e869e37412d
Lautaro Rodrigo Mónaco Assad <lauti.monaco@hotmail.com>
2019-07-28 14:06:16 -0300

    ejercicios while

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,7 +2,7 @@ function mostrar() {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
-	while (numero < 0 || numero > 10) {
+	while (numero < 0 || numero > 9) {
 
 		numero = prompt("Error, ingrese nuevamente el numero");
 	}
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,8 +2,10 @@ function mostrar() {
 
     var sexo = prompt("ingrese f ó m .");
 
+    while (sexo!= "f" && sexo !="m") {
 
-
+        sexo = prompt("Error, ingrese nuevamente el dato");
+    }
     document.getElementById('Sexo').value = sexo;
 
 }//FIN DE LA FUNCIÓN
